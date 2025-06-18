eec7d3341c10488618bce925f8e4e17e0c7c2f1c
Paweł Szynal <pawel@szynal.info>
2025-06-18 23:58:02 +0200

    (abstractions): add CQRS abstractions and base interfaces

---
+++ InsERT.CurrencyApp/src/InsERT.CurrencyApp.Abstractions/CQRS/Commands/ICommand.cs
@@ -0,0 +1,6 @@
+﻿namespace InsERT.CurrencyApp.Abstractions.CQRS.Commands;
+
+/// <summary>
+/// Marker interface for a command that returns a result.
+/// </summary>
+public interface ICommand<TResult> { }
---
+++ InsERT.CurrencyApp/src/InsERT.CurrencyApp.Abstractions/CQRS/Commands/ICommandHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace InsERT.CurrencyApp.Abstractions.CQRS.Commands;
+
+/// <summary>
+/// Handler for a specific command.
+/// </summary>
+public interface ICommandHandler<in TCommand, TResult>
+    where TCommand : ICommand<TResult>
+{
+    Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
+}
