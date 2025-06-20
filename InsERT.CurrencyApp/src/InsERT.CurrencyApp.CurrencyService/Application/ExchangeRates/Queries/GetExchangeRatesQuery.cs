8a2b3eac78e5b860afb97fd0ac9676233f0d1388
Paweł Szynal <pawel@szynal.info>
2025-06-20 13:32:56 +0200

    currency-service: Added handlers, queries and models

---
+++ InsERT.CurrencyApp/src/InsERT.CurrencyApp.CurrencyService/Application/ExchangeRates/Queries/GetExchangeRatesQuery.cs
@@ -0,0 +1,7 @@
+﻿using InsERT.CurrencyApp.Abstractions.CQRS.Queries;
+using InsERT.CurrencyApp.CurrencyService.Application.ExchangeRates.Models;
+
+namespace InsERT.CurrencyApp.CurrencyService.Application.ExchangeRates.Queries;
+
+public sealed record GetExchangeRatesQuery(DateOnly? Date, string? Code)
+    : IQuery<IEnumerable<ExchangeRateDto>>;
